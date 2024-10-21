2962e54cbecf57329f43ce4490b327cc1cea70c6
daisygith <daisygith@users.noreply.github.com>
2024-10-21 12:46:28 +0200

    created dashboard component

---
+++ hr-client/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+import {ToolbarLeftComponent} from "../../../shared/toolbar-left/toolbar-left.component";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ToolbarLeftComponent
+  ],
+  templateUrl: "./dashboard.component.html",
+  styleUrl: "./dashboard.component.html"
+})
+
+export class DashboardComponent {
+
+}
