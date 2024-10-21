1ecf707a2e117c967e535aa8275b63304d4d44ff
daisygith <daisygith@users.noreply.github.com>
2024-10-21 12:46:06 +0200

    added dashboard to router

--- hr-client/src/app/app.routes.ts
+++ hr-client/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/components/dashboard.routes').then(m => m.dashboardRoutes),
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/components/login.routes').then(m=>m.routes),
   },
