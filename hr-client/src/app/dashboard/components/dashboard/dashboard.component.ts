9cda1c977ad76c0a7964e88fb7101e998d9b070b
daisygith <daisygith@users.noreply.github.com>
2024-10-22 11:04:58 +0200

    added toolbars to dashboard

--- hr-client/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ hr-client/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,14 +1,18 @@
 import {Component} from "@angular/core";
-import {ToolbarLeftComponent} from "../../../shared/toolbar-left/toolbar-left.component";
+import {ToolbarLeftComponent} from "../../../shared/components/toolbar-left/toolbar-left.component";
+import {ToolbarTopComponent} from "../../../shared/components/toolbar-top/toolbar-top.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    ToolbarLeftComponent
+    ToolbarLeftComponent,
+    ToolbarTopComponent,
+    RouterOutlet
   ],
   templateUrl: "./dashboard.component.html",
-  styleUrl: "./dashboard.component.html"
+  styleUrl: "./dashboard.component.scss"
 })
 
 export class DashboardComponent {
