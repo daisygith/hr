a3cc7f4fb5ed194fbd3da263e7a4a5362fee6676
daisygith <daisygith@users.noreply.github.com>
2024-10-23 08:03:36 +0200

    added employee to routes

--- hr-client/src/app/app.routes.ts
+++ hr-client/src/app/app.routes.ts
@@ -1,14 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
+  {
+  path: 'login',
+  loadChildren: () => import('./login/components/login.routes').then(m=>m.routes),
+  },
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/components/dashboard.routes').then(m => m.dashboardRoutes),
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/components/login.routes').then(m=>m.routes),
+    path: 'employee',
+    loadChildren: () => import('./employee/components/employee.routes').then(m => m.employeeRoutes),
   },
+
   {
     path: '',
     pathMatch: "full",
