d2c4f6504edd18b2b48fbeba3f953518f51bf146
daisygith <daisygith@users.noreply.github.com>
2024-10-24 09:42:25 +0200

    added new to routes employee - add employee

--- hr-client/src/app/employee/components/employee.routes.ts
+++ hr-client/src/app/employee/components/employee.routes.ts
@@ -4,5 +4,9 @@ export const employeeRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./employee/employee.component').then(m => m.EmployeeComponent),
+  },
+  {
+    path: 'new',
+    loadComponent: () => import('./add-employee/add-employee.component').then(m => m.AddEmployeeComponent),
   }
 ];
