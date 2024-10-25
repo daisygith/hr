2aa6e7cc27d2700ddb3c4c7c0bc89a4469c091a4
daisygith <daisygith@users.noreply.github.com>
2024-10-25 13:52:27 +0200

    imports some stuff to add-employee

--- hr-client/src/app/employee/components/add-employee/add-employee.component.ts
+++ hr-client/src/app/employee/components/add-employee/add-employee.component.ts
@@ -2,6 +2,12 @@ import {Component} from "@angular/core";
 import {RouterOutlet} from "@angular/router";
 import {ToolbarLeftComponent} from "../../../shared/components/toolbar-left/toolbar-left.component";
 import {ToolbarTopComponent} from "../../../shared/components/toolbar-top/toolbar-top.component";
+import {NgOptimizedImage} from "@angular/common";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-add-employee',
@@ -9,7 +15,16 @@ import {ToolbarTopComponent} from "../../../shared/components/toolbar-top/toolba
   imports: [
     RouterOutlet,
     ToolbarLeftComponent,
-    ToolbarTopComponent
+    ToolbarTopComponent,
+    NgOptimizedImage,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelect,
+    MatOption,
+    MatIconButton
   ],
   templateUrl: "./add-employee.component.html",
   styleUrl: "./add-employee.component.scss"
