4a45c0aeed80981f1d53a45219533ad0b6c4d378
daisygith <daisygith@users.noreply.github.com>
2024-10-28 09:24:03 +0100

    changed name from requestTimeOff to request-time-off

---
+++ hr-client/src/app/employee/components/request-time-off/request-time-off.component.ts
@@ -0,0 +1,66 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from '@angular/material/table';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+// TODO: services/employees.service.ts a w nim metodę getTimeOffRequests(), która zwróci tą listę
+const ELEMENT_DATA2: {
+  employeeName: string;
+  leaveType: string;
+  days: number;
+  statusEmployee: string;
+  leaveFrom: string;
+}[] = [
+  {
+    employeeName: 'Test1 Test1',
+    leaveType: 'annual leave',
+    leaveFrom: '2024-03-03',
+    days: 1,
+    statusEmployee: 'pending',
+  },
+];
+
+@Component({
+  selector: 'app-request-time-off', // TODO: nazwa
+  standalone: true,
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatButton,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatIcon,
+  ],
+  templateUrl: './request-time-off.component.html',
+  styleUrl: './request-time-off.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class RequestTimeOffComponent {
+  // TODO: usunąć "2"
+  displayedColumns2: string[] = [
+    'employeeName',
+    'leaveType',
+    'leaveFrom',
+    'days',
+    'statusEmployee',
+    'edit',
+  ];
+  dataSource2 = ELEMENT_DATA2;
+}
