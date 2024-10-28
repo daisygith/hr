542a40e40e82879f21efece55e9c0eb94c2860f1
daisygith <daisygith@users.noreply.github.com>
2024-10-28 07:55:02 +0100

    changes routes for application

--- hr-client/src/app/app.routes.ts
+++ hr-client/src/app/app.routes.ts
@@ -1,22 +1,32 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-  path: 'login',
-  loadChildren: () => import('./login/components/login.routes').then(m=>m.routes),
+    path: 'login',
+    loadChildren: () => import('./login/login.routes').then((m) => m.routes),
   },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/components/dashboard.routes').then(m => m.dashboardRoutes),
-  },
-  {
-    path: 'employee',
-    loadChildren: () => import('./employee/components/employee.routes').then(m => m.employeeRoutes),
-  },
-
   {
     path: '',
-    pathMatch: "full",
-    redirectTo: '/login'
-  }
+    loadComponent: () =>
+      import('./shell/components/shell/shell.component').then(
+        (c) => c.ShellComponent,
+      ),
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./dashboard/dashboard.routes').then((m) => m.dashboardRoutes),
+      },
+      {
+        path: 'employee',
+        loadChildren: () =>
+          import('./employee/employee.routes').then((m) => m.employeeRoutes),
+      },
+    ],
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login',
+  },
 ];
