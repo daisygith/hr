d474bfb41cecff7c2b9e58c28a6b042d61d68c26
daisygith <daisygith@users.noreply.github.com>
2024-10-28 07:59:39 +0100

    changed imports

--- hr-client/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ hr-client/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
 import {Component} from "@angular/core";
-import {ToolbarLeftComponent} from "../../../shared/components/toolbar-left/toolbar-left.component";
-import {ToolbarTopComponent} from "../../../shared/components/toolbar-top/toolbar-top.component";
 import {RouterOutlet} from "@angular/router";
+import {ShellComponent} from "../../../shell/components/shell/shell.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    ToolbarLeftComponent,
-    ToolbarTopComponent,
-    RouterOutlet
+    RouterOutlet,
+    ShellComponent
   ],
   templateUrl: "./dashboard.component.html",
   styleUrl: "./dashboard.component.scss"
