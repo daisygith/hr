06e23386f567f9dd4e71789e832cc97106cbcd63
daisygith <daisygith@users.noreply.github.com>
2024-10-28 09:22:02 +0100

    changed imports

--- hr-client/src/app/employee/components/employee/employee.component.ts
+++ hr-client/src/app/employee/components/employee/employee.component.ts
@@ -1,13 +1,13 @@
-import {Component, ViewEncapsulation} from "@angular/core";
-import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
-import {MatButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {InputComponent} from "../../../shared/components/input/input.component";
-import {MatTableModule} from "@angular/material/table";
-import {ManageEmployeeComponent} from "../manageEmployee/manageEmployee.component";
-import {RequestTimeOffComponent} from "../requestTimeOff/requestTimeOff.component";
-import {ShellComponent} from "../../../shell/components/shell/shell.component";
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { InputComponent } from '../../../shared/components/input/input.component';
+import { MatTableModule } from '@angular/material/table';
+import { ManageEmployeeComponent } from '../manage-employee/manage-employee.component';
+import { ShellComponent } from '../../../shell/components/shell/shell.component';
+import { RequestTimeOffComponent } from '../request-time-off/request-time-off.component';
 
 @Component({
   selector: 'app-employee',
@@ -24,14 +24,11 @@ import {ShellComponent} from "../../../shell/components/shell/shell.component";
     RequestTimeOffComponent,
     RouterLink,
     RouterLinkActive,
-    ShellComponent
+    ShellComponent,
+    RequestTimeOffComponent,
   ],
-  templateUrl: "./employee.component.html",
-  styleUrl: "./employee.component.scss",
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './employee.component.html',
+  styleUrl: './employee.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-
-export class EmployeeComponent {
-
-
-}
+export class EmployeeComponent {}
