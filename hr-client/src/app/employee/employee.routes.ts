f623061c3bce210f7a16f07ba1c9ef6eb89ca37b
daisygith <daisygith@users.noreply.github.com>
2024-10-28 07:55:31 +0100

    changes localization routes file

---
+++ hr-client/src/app/employee/employee.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const employeeRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/employee/employee.component').then(
+        (m) => m.EmployeeComponent,
+      ),
+  },
+  {
+    path: 'new',
+    loadComponent: () =>
+      import('./components/add-employee/add-employee.component').then(
+        (m) => m.AddEmployeeComponent,
+      ),
+  },
+];
