1e8126eb6a0674788de6338e859c4e8908abaa7a
daisygith <daisygith@users.noreply.github.com>
2024-10-29 09:31:10 +0100

    added translate module

--- hr-client/src/app/shared/components/input/input.component.ts
+++ hr-client/src/app/shared/components/input/input.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewEncapsulation} from "@angular/core";
 import {MatFormField, MatInput} from '@angular/material/input';
 import {MatIcon} from "@angular/material/icon";
+import {TranslateModule} from "@ngx-translate/core";
 
 
 @Component({
@@ -8,13 +9,7 @@ import {MatIcon} from "@angular/material/icon";
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
   standalone: true,
-    imports: [
-        MatFormField,
-        MatInput,
-        MatIcon
-    ],
-  encapsulation: ViewEncapsulation.None
+  imports: [MatFormField, MatInput, MatIcon, TranslateModule],
+  encapsulation: ViewEncapsulation.None,
 })
-export class InputComponent {
-
-}
+export class InputComponent {}
