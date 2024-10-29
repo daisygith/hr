0c706be65d9c30d034f4e60b1249565b929d5190
daisygith <daisygith@users.noreply.github.com>
2024-10-29 11:48:27 +0100

    added translate texx; added label to choice language

--- hr-client/src/app/login/components/login/login.component.ts
+++ hr-client/src/app/login/components/login/login.component.ts
@@ -1,19 +1,24 @@
-import {Component, ViewEncapsulation} from "@angular/core";
-import {InputError} from "../../../shared/components/input-error/input-error.component";
-import {MatButton} from "@angular/material/button";
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { InputError } from '../../../shared/components/input-error/input-error.component';
+import { MatButton } from '@angular/material/button';
+import { NgForOf } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     InputError,
-    MatButton
+    MatButton,
+    NgForOf,
+    ReactiveFormsModule,
+    TranslateModule,
   ],
-  templateUrl: "./login.component.html",
-  styleUrl: "./login.component.scss",
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-
 export class LoginComponent {
-
+  public translate: TranslateService = inject(TranslateService);
 }
