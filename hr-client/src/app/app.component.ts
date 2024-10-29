3c8705df94f376c84bcf1aa0e8c63e2836bf0460
daisygith <daisygith@users.noreply.github.com>
2024-10-29 09:30:59 +0100

    added translate service

--- hr-client/src/app/app.component.ts
+++ hr-client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'hr-client';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'pl']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    if (browserLang != null) {
+      translate.use(browserLang.match(/pl|en/) ? browserLang : 'en');
+    }
+  }
 }
