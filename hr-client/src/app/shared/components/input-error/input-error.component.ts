6d8e62a5f83bb6a3887dc9b782d2867e7ecc32aa
daisygith <daisygith@users.noreply.github.com>
2024-10-29 11:47:58 +0100

    added translate text

--- hr-client/src/app/shared/components/input-error/input-error.component.ts
+++ hr-client/src/app/shared/components/input-error/input-error.component.ts
@@ -1,26 +1,31 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {TranslateModule} from "@ngx-translate/core";
 
 /**
  * @title Input with error messages
  */
 @Component({
-    selector: 'input-error',
-    templateUrl: './input-error.component.html',
-    styleUrl: './input-error.component.scss',
-    standalone: true,
+  selector: 'input-error',
+  templateUrl: './input-error.component.html',
+  styleUrl: './input-error.component.scss',
+  standalone: true,
   imports: [
     ReactiveFormsModule,
     MatLabel,
     MatInput,
     MatError,
     MatFormField,
+    TranslateModule,
   ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class InputError {
-    emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-    passwordFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [Validators.required]);
 }
