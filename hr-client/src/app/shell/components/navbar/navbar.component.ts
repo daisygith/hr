96da0d7b767332f3ab9440102dac545e36648ff4
daisygith <daisygith@users.noreply.github.com>
2024-10-29 09:31:43 +0100

    added translate select html; and added inject translate service

--- hr-client/src/app/shell/components/navbar/navbar.component.ts
+++ hr-client/src/app/shell/components/navbar/navbar.component.ts
@@ -1,19 +1,27 @@
-import {Component} from "@angular/core";
-import {InputComponent} from "../../../shared/components/input/input.component";
-
+import { Component, inject } from '@angular/core';
+import { InputComponent } from '../../../shared/components/input/input.component';
+import { MatFormField } from '@angular/material/form-field';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     InputComponent,
-    InputComponent
-
+    InputComponent,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    ReactiveFormsModule,
+    TranslateModule,
+    NgForOf,
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-
 export class NavbarComponent {
-
+  public translate: TranslateService = inject(TranslateService);
 }
