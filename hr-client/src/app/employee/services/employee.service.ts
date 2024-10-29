5c4697c2ba32101f788ec868a3edce5dcaddfc24
daisygith <daisygith@users.noreply.github.com>
2024-10-29 14:26:37 +0100

    created employee service

---
+++ hr-client/src/app/employee/services/employee.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { RequestTimeOff } from '../models/requestTimeOff';
+import { ManageEmployee } from '../models/manageEmmployee';
+
+@Injectable({ providedIn: 'root' })
+export class EmployeeService {
+  public getManageEmployee(): ManageEmployee[] {
+    return [
+      {
+        employeeName: 'Test1 Test1',
+        phoneNumber: '(+62) 928 7273 7262',
+        department: 'Design',
+        jobTitle: 'Creative Direction',
+        contactType: 'Onsite - Full time',
+        attendance: '120h 32m',
+      },
+    ];
+  }
+
+  public getRequestTimeOff(): RequestTimeOff[] {
+    return [
+      {
+        employeeName: 'Test1 Test1',
+        leaveType: 'annual leave',
+        leaveFrom: '2024-03-03',
+        days: 1,
+        statusEmployee: 'pending',
+      },
+    ];
+  }
+}
