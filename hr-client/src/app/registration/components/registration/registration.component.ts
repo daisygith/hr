e0ca0eef466044c37a169369915594041ecb9075
daisygith <daisygith@users.noreply.github.com>
2024-11-05 10:36:34 +0100

    added some changes to registration component

---
+++ hr-client/src/app/registration/components/registration/registration.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject, signal, ViewEncapsulation } from '@angular/core';
+import { InputError } from '../../../shared/components/input-error/input-error.component';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { NgForOf } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatFormField, MatSuffix } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput, MatLabel } from '@angular/material/input';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [
+    InputError,
+    MatButton,
+    NgForOf,
+    ReactiveFormsModule,
+    TranslateModule,
+    MatFormField,
+    MatIcon,
+    MatInput,
+    MatLabel,
+    MatIconButton,
+    MatSuffix,
+    RouterLink,
+    RouterLinkActive,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger,
+  ],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class RegistrationComponent {
+  public translate: TranslateService = inject(TranslateService);
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  hideConfirm = signal(true);
+  clickEventConfirm(event: MouseEvent) {
+    this.hideConfirm.set(!this.hideConfirm());
+    event.stopPropagation();
+  }
+}
