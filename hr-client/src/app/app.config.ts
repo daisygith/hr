aace10af4151c7a8dc5639de21af6afae7ff54f1
daisygith <daisygith@users.noreply.github.com>
2024-11-06 09:30:43 +0100

    added auth stuff

--- hr-client/src/app/app.config.ts
+++ hr-client/src/app/app.config.ts
@@ -3,9 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  provideHttpClient,
+} from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { JwtInterceptor } from './auth/services/jwtInterceptor';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -25,6 +30,7 @@ export const appConfig: ApplicationConfig = {
       }),
     ),
     provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     // providers: [provideHttpClient()]
   ],
 };
