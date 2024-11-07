3a0926e8cae2c48a1732e1b78328e110ff3e95e6
daisygith <daisygith@users.noreply.github.com>
2024-11-07 13:11:15 +0100

    created formGroup; sent data to backend; added snackBar for successfully or wrong registration

--- hr-client/src/app/auth/components/registration/components/registration/registration.component.ts
+++ hr-client/src/app/auth/components/registration/components/registration/registration.component.ts
@@ -1,13 +1,32 @@
-import { Component, inject, signal, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { NgForOf } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { MatFormField, MatSuffix } from '@angular/material/form-field';
+import {
+  MatError,
+  MatFormField,
+  MatSuffix,
+} from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput, MatLabel } from '@angular/material/input';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { passwordMismatchDirective } from '../../../../../shared/password-mismatch.directive';
+import { AuthService } from '../../../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-registration',
@@ -28,13 +47,90 @@ import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
     MatMenu,
     MatMenuItem,
     MatMenuTrigger,
+    MatError,
   ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
   public translate: TranslateService = inject(TranslateService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public registrationForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public buildForm() {
+    this.registrationForm = this.fb.group(
+      {
+        username: new FormControl('', Validators.required),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/[a-z0-9\._%\+\-]+@[a-z0-9\.\-]+[a-z]{2,}$/),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.pattern(
+            /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{6,40}$/,
+          ),
+        ]),
+        confirmPassword: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: passwordMismatchDirective,
+      },
+    );
+  }
+
+  onRegistration() {
+    const userDetails = { ...this.registrationForm.value };
+
+    delete userDetails.confirmPassword;
+
+    this.authService.registration(userDetails).subscribe({
+      next: (response) => {
+        this._snackBar.open('Registered successfully', 'OK', {
+          duration: 3000,
+          panelClass: ['green-snackbar'],
+        });
+        console.log(response);
+        this.router.navigate(['login']);
+      },
+      error: (err) => {
+        console.log(err);
+        this._snackBar.open('Something went wrong', 'X', {
+          duration: 3000,
+          panelClass: ['red-snackbar'],
+        });
+      },
+    });
+  }
+
+  get username() {
+    return this.registrationForm.controls['username'];
+  }
+  get email() {
+    return this.registrationForm.controls['email'];
+  }
+  get password() {
+    return this.registrationForm.controls['password'];
+  }
+  get confirmPassword() {
+    return this.registrationForm.controls['confirmPassword'];
+  }
+
+  // openSnackBar(message: string, type: string) {
+  //   this._snackBar.open('There was an error', 'X');
+  // }
 
   hide = signal(true);
   clickEvent(event: MouseEvent) {
