dd46f949455434a9d7347eb88b1f64ba061e0a97
daisygith <daisygith@users.noreply.github.com>
2024-11-22 10:20:06 +0100

    added error handling

--- hr-server/src/main/java/pl/sdu/hr/exception/ErrorAdvice.java
+++ hr-server/src/main/java/pl/sdu/hr/exception/ErrorAdvice.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @ControllerAdvice
 public class ErrorAdvice {
 
@@ -15,6 +17,13 @@ public class ErrorAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class) //zawiera klase która bedzie obsługiwana
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String MethodArgumentNotValidHandler(MethodArgumentNotValidException ex){
+        return ex.getMessage();
+    }
+
+    @ResponseBody // odp mogła zostać przemapowana do odpowiedzi json-a
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class) //zawiera klase która bedzie obsługiwana
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException ex){
         return ex.getMessage();
     }
 
